a1d36d584bcbe6b8ef083bb8106aa613ec2adcd5
Pauli <pauli.alakylmanen@yahoo.com>
2024-10-08 12:28:06 +0300

    deployed website 4

--- src/components/Navb.js
+++ src/components/Navb.js
@@ -5,7 +5,7 @@ import React from "react";
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
     return (
         <nav>
-            <h1>GeeksforGeeks Musiikkisoitin (https://www.geeksforgeeks.org/)</h1>
+            <h1>GeeksforGeeks Musiikkisoitin <a href="https://www.geeksforgeeks.org/">https://www.geeksforgeeks.org/</a></h1>
             <button
                 onClick={() => {
                     setLibraryStatus(!libraryStatus);
